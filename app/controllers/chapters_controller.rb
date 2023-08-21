0a54c867759a33ac5107ee66a8b627d9636c95e3
rafay-qayyum <rafay.qayyum@pakeventures.com>
2023-08-21 17:07:10 +0500

    Course deletion and updation, Chapter Creation, deletion, updation

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,2 +1,49 @@
 class ChaptersController < ApplicationController
+
+  def index
+    @chapters=Chapter.find(params[:course_id])
+  end
+  def show
+    @chapter=Chapter.find(params[:id])
+  end
+  def new
+    @course=Course.find(params[:course_id])
+    @chapter = @course.chapters.new
+  end
+  def create
+    @course=Course.find(params[:chapter][:course_id])
+    @chapter = @course.chapters.create(chapter_params)
+    if @chapter.save
+      flash[:notice] = "Chapter created successfully"
+      redirect_to course_path(@course)
+    else
+      flash[:alert] = "Chapter not created successfully"
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def edit
+    @chapter = Chapter.find(params[:id])
+    @course = Course.find(params[:course_id])
+  end
+
+  def update
+    @chapter = Chapter.find(params[:id])
+    if @chapter.update(chapter_params)
+      flash[:notice] = "Chapter updated successfully"
+      redirect_to course_path(@chapter.course_id)
+    else
+      flash[:alert] = "Chapter not updated successfully"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @chapter = Chapter.find(params[:id])
+    @chapter.destroy
+    redirect_to course_path(@chapter.course_id)
+  end
+
+  private
+  def chapter_params
+    params.require(:chapter).permit(:name,:content,:quiz,:assignment)
+  end
 end
--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,8 +2,11 @@ class Chapter < ApplicationRecord
   belongs_to :course
   has_one_attached :quiz
   has_one_attached :assignment
-  has_one :chapter_results, dependent: :destroy
+  has_one :chapter_results, dependent: :destroy, :class_name => "ChapterResult"
   validates :name, presence: true, length: {minimum:10, maximum: 50}
   validates :content, presence: true, length: {minimum:40, maximum:800}
 
+  def self.ransackable_attributes(auth_object = nil)
+    ["content", "course_id", "id", "name", "quiz", "assignment"]
+  end
 end
